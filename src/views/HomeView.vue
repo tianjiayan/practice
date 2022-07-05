814cde39b818163d116d0653cc7e26b9450ab4a1
Tian <11132373+tianjiayan@user.noreply.gitee.com>
2022-07-05 09:39:22 +0800

    feat: eslint git 提交方式

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,10 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <el-button type="primary">Primary</el-button>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
 </script>
+<style lang="scss" scoped>
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+module.exports = {
+  devServer: {
+    port: 8888,
+    host: 'localhost',
+    https: false,
+    open: true,
+    proxy: {
+      [process.env.VUE_APP_BASE_API]: {
+        target: process.env.VUE_APP_SERVICE_URL,
+        changeOrigin: true,
+        pathRewrite: {
+          ['^' + process.env.VUE_APP_BASE_API]: ''
+        }
+      }
+    }
+  },
+  lintOnSave: false
+}
