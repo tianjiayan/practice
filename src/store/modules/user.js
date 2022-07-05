0d85a08656acc54a452dab190b5beef6392c299a
Tian <11132373+tianjiayan@user.noreply.gitee.com>
2022-07-05 18:53:41 +0800

    feat: dist?

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -3,12 +3,16 @@ import user from '../../api/user'
 export default {
   namespaced: true,
   state: () => ({
-    token: getItem('token') || ''
+    token: getItem('token') || '',
+    userInfo: {}
   }),
   mutations: {
     setToken(state, token) {
       state.token = token
       setItem('token', token)
+    },
+    setUserInfo(state, userInfo) {
+      state.userInfo = userInfo
     }
   },
   actions: {
@@ -22,6 +26,15 @@ export default {
     //     }
     // }
 
+    async getUserInfo({ commit }) {
+      try {
+        const response = await user.getUserInfo()
+        commit('setUserInfo', response)
+        return response
+      } catch (err) {
+        console.log(err)
+      }
+    },
     logout({ commit }) {
       // resetRouter(), commit('setToken', '')
       commit('setUserInfo', {})
